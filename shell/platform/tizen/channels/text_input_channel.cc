4457080caeb1489a8b7354026156159315a9edc4
Swift Kim <swift.kim@samsung.com>
2021-04-13 16:14:29 +0900

    Add the common profile

--- shell/platform/tizen/channels/text_input_channel.cc
+++ shell/platform/tizen/channels/text_input_channel.cc
@@ -277,10 +277,10 @@ TextInputChannel::TextInputChannel(flutter::BinaryMessenger* messenger,
     imf_context_ = ecore_imf_context_add(GetImfMethod());
   }
   if (imf_context_) {
-    // TODO(swift-kim): Incorrect cast from int to void*.
+    // Caution: Conversion between incompatible types (int to void*).
     ecore_imf_context_client_window_set(
         imf_context_,
-        (void*)(size_t)(engine_->tizen_renderer->GetEcoreWindowId()));
+        (void*)(intptr_t)(engine_->tizen_renderer->GetEcoreWindowId()));
     RegisterIMFCallback();
   } else {
     FT_LOGE("Failed to create imfContext");
