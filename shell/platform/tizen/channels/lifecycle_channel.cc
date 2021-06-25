ad787111f2031f56711f888a6a6e0e5e56df2f58
Swift Kim <swift.kim@samsung.com>
2021-06-25 16:16:07 +0900

    Remove StringSerializer and implement StringCodec

--- shell/platform/tizen/channels/lifecycle_channel.cc
+++ shell/platform/tizen/channels/lifecycle_channel.cc
@@ -4,9 +4,7 @@
 
 #include "lifecycle_channel.h"
 
-#include <variant>
-
-#include "flutter/shell/platform/common/client_wrapper/include/flutter/standard_message_codec.h"
+#include "flutter/shell/platform/tizen/channels/string_codec.h"
 #include "flutter/shell/platform/tizen/tizen_log.h"
 
 namespace flutter {
@@ -20,38 +18,13 @@ constexpr char kResumed[] = "AppLifecycleState.resumed";
 constexpr char kPaused[] = "AppLifecycleState.paused";
 constexpr char kDetached[] = "AppLifecycleState.detached";
 
-// Codec extension for UTF-8 strings.
-class StringSerializer : public StandardCodecSerializer {
- public:
-  StringSerializer() = default;
-  virtual ~StringSerializer() = default;
-
-  // Returns the shared serializer instance.
-  static const StringSerializer& GetInstance() {
-    static StringSerializer sInstance;
-    return sInstance;
-  }
-
-  virtual void WriteValue(const EncodableValue& value,
-                          ByteStreamWriter* stream) const override {
-    if (auto string_value = std::get_if<std::string>(&value)) {
-      size_t size = string_value->size();
-      if (size > 0) {
-        stream->WriteBytes(
-            reinterpret_cast<const uint8_t*>(string_value->data()), size);
-      }
-    }
-  }
-};
-
 }  // namespace
 
 LifecycleChannel::LifecycleChannel(BinaryMessenger* messenger)
     : channel_(std::make_unique<BasicMessageChannel<EncodableValue>>(
           messenger,
           kChannelName,
-          &StandardMessageCodec::GetInstance(
-              &StringSerializer::GetInstance()))) {}
+          &StringCodec::GetInstance())) {}
 
 LifecycleChannel::~LifecycleChannel() {}
 
